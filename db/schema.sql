5b7ed614b614f824f9a55fa42d62e42c7c1626e5
Jason <el.jsteer@gmail.com>
2022-08-06 15:52:59 +1000

    added db connection and function to view EMPloyees and db table viewer DL

--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT
+  role_id INT,
+  manager_id INT,
   -- PRIMARY KEY (id),
   FOREIGN KEY role_id,
   REFERENCES role(id),
--- index.js
+++ index.js
@@ -1,3 +1,76 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const mysql = require("mysql2");
+require("dotenv").config();
+const cTable = require('console.table');
+const Employee = require("./db/organisation");
+const Role = require("./db/organisation");
+const Department = require("./db/organisation");
+
+const db = mysql.createConnection(
+  process.env.HOST,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  console.log("Connected to the Tracker_DB"),
+)
+
+function validateInput(data) {
+  if(data != "") {
+    return true;
+  } else {
+    return "Please enter a response to the query"
+  }
+};
+
+function mainMenu() {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "mainMenu",
+      message: "What Action Would You Like To Take?", 
+      choices: [
+      "View All Employees", 
+      "Add Employee", 
+      "Update Employee Role",
+      "View All Roles",
+      "Add Job Role",
+      "View All Departments",
+      "Add Department",
+      "Update Manager",
+      "View Employees by Filter"
+      ],
+      validate: validateInput,
+    }
+  ])
+    .then(data => {
+      switch (data.mainMenu) {
+        case 'View All Employees':
+          Employee.viewEmployees();
+          break;
+        case 'Add Employee':
+          Employee.addEmployee();
+          break;
+        case 'Update Employee Role':
+          Employee.updateEmployeeRole();
+          break;
+        case 'View All Roles':
+          Role.viewRoles();
+          break;
+        case 'Add Job Role':
+          Role.addJobRole();
+          break;
+        case 'View All Departments':
+          Department.viewDepartments();
+          break;
+        case 'Add Department':
+          Department.addDepartment();
+          break;
+      };
+    });
+};
+
+function init() {
+  mainMenu();
+};
+
+init();
