5d04dbf16eec40672829508299a19af29c0ce933
Jason <el.jsteer@gmail.com>
2022-08-10 20:47:38 +1000

    ViewRoles working with adding extra row and view all departments and add department

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function mainMenu() {
         "View All Employees", 
         "View All Roles",
         "Add a Department",
-        "Add a Employee",
+        "Add an Employee",
         "Add a Role", 
         "Update Employees Role",
         "Update Employees Managers",
@@ -51,10 +51,10 @@ function mainMenu() {
         case 'Add a Department':
           addDepartment();
           break;
-        case 'Add Employee':
+        case 'Add an Employee':
           addEmployee();
           break;
-        case 'Add Role':
+        case 'Add a Role':
           addJobRole();
           break;
         case 'Update Employees Role':
@@ -86,6 +86,74 @@ function mainMenu() {
 // Functions to Display Tracker Tables & Process Data
 //=================================================>>
 
+// Function to View All Departments
+async function viewDep() {
+  let depData = await dbQuery.viewDepartments();
+  console.table(depData[0]);
+  mainMenu();
+};
+
+// Function to add a Department
+addDepartment = async () => {
+  try {
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "depName",
+      message: "What is the Name of the new Department?", 
+      validate: validateInput,
+      },
+  ]);
+  console.log(answers);
+  await dbQuery.addDepartment([answers]);
+  } catch (err) {
+    console.log(err);
+  };
+  mainMenu();
+};
+
+// Function to View all Roles
+async function viewRoles() {
+  let roleData = await dbQuery.viewRoles();
+  console.table(roleData[0]);
+  mainMenu();
+};
+
+// Function to add a Job Role
+addJobRole = async () => {
+  try {
+  let depArr = await dbQuery.viewDepartments();
+  depOpt = depArr[0].map(x => x.dep_name);
+  const answers = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the Name of the new Role?", 
+      validate: validateInput,
+      },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the Salary of the new Role?", 
+      validate: validateInput,
+      },
+    {
+      type: "list",
+      name: "roleDep",
+      message: "Which department does the role belong to?",
+      choices: depOpt,
+      validate: validateInput,
+      },
+  ]);
+  console.log(answers);
+  await dbQuery.addJobRole(answers);
+  } catch (err) {
+    console.log(err);
+  };
+  mainMenu();
+};
+
+// Function to Add a New Employee
 addEmployee = () => {
   return inquirer.prompt([
     {
@@ -116,6 +184,8 @@ addEmployee = () => {
       mainMenu();
     })
 };
+
+// Function to update an existing Employee's Role
 updateEmployeeRole = () => {
   return inquirer.prompt([
     {
@@ -128,61 +198,14 @@ updateEmployeeRole = () => {
     mainMenu();
   })
 };
-addJobRole = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the Name of the new Role?", 
-      validate: validateInput,
-      },
-    {
-      type: "input",
-      name: "salary",
-      message: "What is the Salary of the new Role?", 
-      validate: validateInput,
-      },
-    {
-      type: "list",
-      name: "roleDep",
-      message: "Which department does the role belong to?", 
-      validate: validateInput,
-      },
-  ]).then((answers)=> {
-      mainMenu();
-  })
-};
-addDepartment = async () => {
-  try {
-  const answers = await inquirer.prompt([
-    {
-      type: "input",
-      name: "depName",
-      message: "What is the Name of the new Department?", 
-      validate: validateInput,
-      },
-  ]);
-  console.log(answers);
-  await dbQuery.addDepartment([answers]);
-  } catch (err) {
-    console.log(err);
-  };
-  mainMenu();
-};
 
 // function promptDataHelper(answers) {
 //   let answerArray = [];
 //   answerArray.push(answers);
 // }
 
-async function viewDep() {
-  let depData = await dbQuery.viewDepartments();
-  console.table(depData[0]);
-  mainMenu();
-};
-
 async function exit() {
-  return prompt.ui.close()
+  return prompt.ui.close();
 };
 
 // const Name = await dbQuery.function();
